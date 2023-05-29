816fbf53d78c13fceb3c70a12f01bdd76ad27cf2
Eashswain <115333645+Eashswain@users.noreply.github.com>
2023-05-29 11:54:47 +0530

    Add files via upload

---
+++ reversearray.c
@@ -0,0 +1,25 @@
+/*hackerrank first problem to rverse the array
+first allocate the position,then using temp interchange the position for even numbers(num/2) and finally print the number*/
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int num,*arr,i;
+    printf("enter the size of an array");
+    scanf("%d",&num);
+    arr=(int*)malloc(num*(sizeof(int)));
+    printf("enter the number");
+    for(i=0;i<num;i++)
+
+    scanf("%d",arr+i);
+
+int temp;
+for(i=0;i<num/2;i++){
+temp=arr[i];
+arr[i]=arr[num-1-i];
+arr[num-1-i]=temp;
+}
+
+for(i=0;i<num;i++){
+    printf("%d",*(arr+i));
+}
+}
